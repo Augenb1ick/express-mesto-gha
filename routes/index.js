c6c02d0f959f44366d6a2f369ca4e7d99d0e9352
Augenb1ick <augenblick14@yandex.ru>
2023-06-22 15:20:03 +0400

    fixed validation bugs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
-const httpConstants = require('http2').constants;
 
 router.use('/users', require('./users'));
 router.use('/cards', require('./cards'));
-
-router.use((req, res) => {
-  res.status(httpConstants.HTTP_STATUS_NOT_FOUND).send({ message: 'Такой страницы не существует' });
-});
 
 module.exports = router;
